ea2a87485ef48c7be87efdd786fdc889210b4632
Hardeep56 <101615491+Hardeep56@users.noreply.github.com>
2024-05-16 10:02:01 -0700

    Add files via upload

---
+++ PrintInfo.java
@@ -0,0 +1,18 @@
+
+public class PrintInfo {
+    public static void main(String[] args) {
+        // Declare variables and assign values
+        String firstName = "Hardeep";
+        String lastName = "Singh";
+        String streetAddress = "237 Columbus Dr";
+        String city = "Manteca";
+        String zipCode = "95337";
+        
+        // Print each variable on a new line
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
